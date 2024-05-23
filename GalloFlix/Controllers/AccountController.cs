166ced38bd1ca7327fdc3579d07b248d03b2d885
bruno-martignago <brunomartignago106@gmail.com>
2024-05-22 21:55:00 -0300

    Finalização de login e logoff

--- GalloFlix/Controllers/AccountController.cs
+++ GalloFlix/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using System.Net.Mail;
+using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 using GalloFlix.ViewModels;
 using Microsoft.AspNetCore.Identity;
@@ -38,12 +39,47 @@ public AccountController(
     {
         if (ModelState.IsValid)
         {
+            string userName = login.Email;
+            if (IsValidEmail(userName))
+            {
+                var user = await _userManager.FindByEmailAsync(userName);
+                if (user != null)
+                    userName = user.UserName;
+            }
 
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Password, login.RememberMe, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {userName} fez login");
+                return LocalRedirect(login.ReturnUrl);
+            }    
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {userName} foi bloqueado");
+                ModelState.AddModelError(string.Empty, "Conta Bloqueada! Aguarde alguns minutos para continuar!");
+            }
+            else
+                ModelState.AddModelError(string.Empty, "Usuário e/ou senha inválidos!");
         }
         return View(login);
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuario {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+    public IActionResult AccessDenied()
+    {
+        return View();
+    }
     public IActionResult Error()
     {
         return View("Error!");
